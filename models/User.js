45f1faf1b0cd6c779f645a330c33b9927f705da1
naomi <naomilounsbury@hotmail.com>
2022-03-05 10:45:51 -0800

    user model

---
+++ models/User.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/],
+    },
+    thought: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Friends",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    // prevents virtuals from creating duplicate of _id as `id`
+    id: false,
+  }
+);
+UserSchema.virtual("friendsCount").get(function () {
+  return this.friends.reduce((total, friends) => total + friends.length + 1, 0);
+});
+
+const User = model("User", UserSchema);
+
+module.exports = User;
